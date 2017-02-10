584e85ec3b8e152efe00735abe7a6d2ab5ea6bb3
Irene <carina.omsk@gmail.com>
2017-02-10 06:33:14 +0000

    Added style for counter

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -19,7 +19,7 @@ class MessageList extends Component {
       <div>
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
-          <span>{this.props.counter} user(s) online</span>
+          <span className="connections">{this.props.counter} user(s) online</span>
         </nav>
         <main className="messages">
           { msgComponents }
